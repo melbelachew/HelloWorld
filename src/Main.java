b14b5b67a0c11c7fb258b3bc44e814602b40c0c4
Mel <melbelachew@gmail.com>
2019-10-21 16:18:18 -0400

    added variable name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args){
+        //Printing out hello world to the console
         System.out.println("Hello World");
+        // declaration a variable
+        String name;
+        //initialization of a variable
+        name ="Mel";
+        //declaration and initialization of a varible in the same line
+        String newName = "Melo";
+        //reinitializing a variable
+        name ="Sam";
+        System.out.print(name);
+        System.out.println(" Last Name");
+
     }
 }
